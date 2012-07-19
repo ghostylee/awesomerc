0e4cccc990b0d758e048e5bb519c669e80706dff
ghosty <ghosty.lee.1984@gmail.com>
2012-07-19 14:18:33 +0800

    change theme colors

--- themes/ghosty/theme.lua
+++ themes/ghosty/theme.lua
@@ -1,75 +1,13 @@
----------------------------
--- "Need_Aspirin" awesome theme --
----------------------------
 --awesome colors
 green="#7fb219"
 cyan="#7f4de6"
-red="#e04613"                             
-lblue="#6c9eab"                        
-dblue="#00ccff"
+red="#e04613"
+lblue="#00afaf"
+dblue="#005fdf"
 black="#000000"
 lgrey="#d2d2d2"
 dgrey="#333333"
 white="#ffffff"
-
-alpha="dd"
-
---bash colors
---add the following lines in your bashrc
-
---#color foreground prompt
---BLACK='\[\e[0;30m\]'
---DGRAY='\[\e[1;30m\]'
---BLUE='\[\e[0;34m\]'
---LBLUE='\[\e[1;34m\]'
---GREEN='\[\e[0;32m\]'
---LGREEN='\[\e[1;32m\]'
---CYAN='\[\e[0;36m\]'
---LCYAN='\[\e[1;36m\]'
---RED='\[\e[0;31m\]'
---LRED='\[\e[1;31m\]'
---PURPLE='\[\e[0;35m\]'
---LPURPLE='\[\e[1;35m\]'
---BROWN='\[\e[0;33m\]'
---YELLOW='\[\e[1;33m\]'
---LGRAY='\[\e[0;37m\]'
---WHITE='\[\e[1;37m\]'
---RESET_COLOR='\[\e[0m\]'
---#color background prompt
---BG_BLACK='\[\e[40m\]'       # Black
---BG_RED='\e[41m'         # Red
---BG_GREEN='\e[42m'       # Green
---BG_YELLOW='\e[43m'      # Yellow
---BG_BLUE='\e[44m'        # Blue
---BG_PURPLE='\e[45m'      # Purple
---BG_CYAN='\e[46m'        # Cyan
---BG_WHITE='\e[47m'       # White
-
---color background prompt
---don't forget to set double\ before \n, \w etc...
-PS1='"\\n${DGRAY}╭─[${LBLUE}\\w${DGRAY}]\\n${DGRAY}╰─[${WHITE}\\T${DGRAY}]${DGRAY}>${BLUE}>${LBLUE}> ${RESET_COLOR}"'
-
---my_bash_profile=home .."/.bashrc"
-tmp_file="/tmp/bash_lua_test.tmp"
---fh,err = io.open(my_bash_profile)
---if err then print("OOps"); return; end
--- Open a file for write
---fho,err = io.open(tmp_file,"w")
---while true do
- --line = fh:read()
- --if line == nil then break end
-    --if string.find(line,"^PS1=") then
-       --fho:write("PS1="..PS1)
-       --fho:write("\n")
-    --else 
-       --fho:write(line) 
-       --fho:write("\n")
-    --end
- --end
---fh:close()
---fho:close()
---os.remove(my_bash_profile)
---os.execute("cp "..tmp_file .." "..my_bash_profile)
 
 theme = {}
 
@@ -81,13 +19,13 @@ theme.bg_urgent     = red
 theme.bg_minimize   = dgrey
 
 theme.fg_normal     = lgrey
-theme.fg_focus      = dblue
+theme.fg_focus      = lblue
 theme.fg_urgent     = white
 theme.fg_minimize   = white
 
 theme.border_width  = "1"
 theme.border_normal = black
-theme.border_focus  = dblue
+theme.border_focus  = lblue
 theme.border_marked = red
 
 -- There are other variable sets
@@ -99,15 +37,12 @@ theme.border_marked = red
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
 -- Example:
 
-theme.taglist_bg_focus = black
-theme.taglist_fg_focus = dblue
-
-theme.tasklist_bg_focus = black
-theme.tasklist_fg_focus = dblue
+theme.tasklist_bg_focus = lblue
+theme.tasklist_fg_focus = black
 
 --font color fort textbox widget used as label in my wibox
-theme.textbox_widget_as_label_font_color = white 
-theme.textbox_widget_margin_top =1 
+theme.textbox_widget_as_label_font_color = white
+theme.textbox_widget_margin_top =1
 
 --font colors for textbox widget
 theme.text_font_color_1 = green
@@ -124,89 +59,89 @@ theme.notify_fg = theme.fg_normal
 theme.notify_bg = theme.bg_normal
 theme.notify_border = theme.border_focus
 --colors,height for all awfull widgets
-theme.awful_widget_bckgrd_color = dgrey 
-theme.awful_widget_border_color = dgrey 
+theme.awful_widget_bckgrd_color = dgrey
+theme.awful_widget_border_color = dgrey
 theme.awful_widget_color= dblue
-theme.awful_widget_gradien_color_1= orange 
+theme.awful_widget_gradien_color_1= orange
 theme.awful_widget_gradien_color_2= orange
 theme.awful_widget_gradien_color_3= orange
 theme.awful_widget_height=14
-theme.awful_widget_margin_top=2 
-
+theme.awful_widget_margin_top=2
+config_dir = awful.util.getdir("config")
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/home/ghosty/.config/awesome/themes/ghosty/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/home/ghosty/.config/awesome/themes/ghosty/taglist/squarew.png"
+theme.taglist_squares_sel   = config_dir .. "/themes/ghosty/taglist/squarefw.png"
+theme.taglist_squares_unsel = config_dir .. "/themes/ghosty/taglist/squarew.png"
 
-theme.tasklist_floating_icon = "/home/ghosty/.config/awesome/themes/ghosty/tasklist/floatingw.png"
+theme.tasklist_floating_icon = config_dir .. "/themes/ghosty/tasklist/floatingw.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/home/ghosty/.config/awesome/themes/ghosty/submenu.png"
+theme.menu_submenu_icon = config_dir .. "/themes/ghosty/submenu.png"
 theme.menu_height = "12"
 theme.menu_width  = "120"
-theme.menu_border_color = dgrey 
+theme.menu_border_color = dgrey
 theme.menu_border_width = 1
 
 -- You can add as many variables as
 -- you wish and access them by using
 -- beautiful.variable in your rc.lua
---theme.bg_widget = 
+--theme.bg_widget =
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = config_dir .. "/themes/ghosty/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = config_dir .. "/themes/ghosty/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = config_dir .. "/themes/ghosty/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = config_dir .. "/themes/ghosty/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = config_dir .. "/themes/ghosty/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = config_dir .. "/themes/ghosty/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = config_dir .. "/themes/ghosty/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = config_dir .. "/themes/ghosty/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = config_dir .. "/themes/ghosty/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = config_dir .. "/themes/ghosty/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = config_dir .. "/themes/ghosty/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = config_dir .. "/themes/ghosty/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = config_dir .. "/themes/ghosty/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = config_dir .. "/themes/ghosty/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/home/ghosty/.config/awesome/themes/ghosty/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = config_dir .. "/themes/ghosty/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = config_dir .. "/themes/ghosty/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = config_dir .. "/themes/ghosty/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = config_dir .. "/themes/ghosty/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg ".. "/home/ghosty/.config/awesome/themes/ghosty/background.png"}
+theme.wallpaper_cmd = { "awsetbg ".. config_dir .. "/themes/ghosty/background.png"}
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/home/ghosty/.config/awesome/themes/ghosty/layouts/fairhw.png"
-theme.layout_fairv = "/home/ghosty/.config/awesome/themes/ghosty/layouts/fairvw.png"
-theme.layout_floating  = "/home/ghosty/.config/awesome/themes/ghosty/layouts/floatingw.png"
-theme.layout_magnifier = "/home/ghosty/.config/awesome/themes/ghosty/layouts/magnifierw.png"
-theme.layout_max = "/home/ghosty/.config/awesome/themes/ghosty/layouts/maxw.png"
-theme.layout_fullscreen = "/home/ghosty/.config/awesome/themes/ghosty/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/home/ghosty/.config/awesome/themes/ghosty/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/home/ghosty/.config/awesome/themes/ghosty/layouts/tileleftw.png"
-theme.layout_tile = "/home/ghosty/.config/awesome/themes/ghosty/layouts/tilew.png"
-theme.layout_tiletop = "/home/ghosty/.config/awesome/themes/ghosty/layouts/tiletopw.png"
-theme.layout_spiral  = "/home/ghosty/.config/awesome/themes/ghosty/layouts/spiralw.png"
-theme.layout_dwindle = "/home/ghosty/.config/awesome/themes/ghosty/layouts/dwindlew.png"
+theme.layout_fairh = config_dir .. "/themes/ghosty/layouts/fairhw.png"
+theme.layout_fairv = config_dir .. "/themes/ghosty/layouts/fairvw.png"
+theme.layout_floating  = config_dir .. "/themes/ghosty/layouts/floatingw.png"
+theme.layout_magnifier = config_dir .. "/themes/ghosty/layouts/magnifierw.png"
+theme.layout_max = config_dir .. "/themes/ghosty/layouts/maxw.png"
+theme.layout_fullscreen = config_dir .. "/themes/ghosty/layouts/fullscreenw.png"
+theme.layout_tilebottom = config_dir .. "/themes/ghosty/layouts/tilebottomw.png"
+theme.layout_tileleft   = config_dir .. "/themes/ghosty/layouts/tileleftw.png"
+theme.layout_tile = config_dir .. "/themes/ghosty/layouts/tilew.png"
+theme.layout_tiletop = config_dir .. "/themes/ghosty/layouts/tiletopw.png"
+theme.layout_spiral  = config_dir .. "/themes/ghosty/layouts/spiralw.png"
+theme.layout_dwindle = config_dir .. "/themes/ghosty/layouts/dwindlew.png"
 
-theme.awesome_icon = "/home/ghosty/.config/awesome/themes/ghosty/awesome.png"
-theme.mpdicon = "/home/ghosty/.config/awesome/themes/ghosty/icons/note-48x48.png"
-theme.shortcut = "/home/ghosty/.config/awesome/themes/ghosty/icons/keyboard_shortcut.png"
-theme.shutdown = "/home/ghosty/.config/awesome/themes/ghosty/icons/shutdown.png"
-theme.reboot = "/home/ghosty/.config/awesome/themes/ghosty/icons/reboot.png"
-theme.accept = "/home/ghosty/.config/awesome/themes/ghosty/icons/accept.png"
-theme.cancel = "/home/ghosty/.config/awesome/themes/ghosty/icons/cancel.png"
-theme.calendar = "/home/ghosty/.config/awesome/themes/ghosty/icons/calendar.png"
-theme.task = "/home/ghosty/.config/awesome/themes/ghosty/icons/task.png"
-theme.tasks = "/home/ghosty/.config/awesome/themes/ghosty/icons/tasks.png"
-theme.task_done = "/home/ghosty/.config/awesome/themes/ghosty/icons/task_done.png"
-theme.project = "/home/ghosty/.config/awesome/themes/ghosty/icons/project.png"
-theme.udisks_glue = "/home/ghosty/.config/awesome/themes/ghosty/icons/udisk-glue.png"
-theme.usb = "/home/ghosty/.config/awesome/themes/ghosty/icons/usb.png"
-theme.cdrom = "/home/ghosty/.config/awesome/themes/ghosty/icons/cdrom.png"
+theme.awesome_icon = config_dir .. "/themes/ghosty/awesome.png"
+theme.mpdicon = config_dir .. "/themes/ghosty/icons/note-48x48.png"
+theme.shortcut = config_dir .. "/themes/ghosty/icons/keyboard_shortcut.png"
+theme.shutdown = config_dir .. "/themes/ghosty/icons/shutdown.png"
+theme.reboot = config_dir .. "/themes/ghosty/icons/reboot.png"
+theme.accept = config_dir .. "/themes/ghosty/icons/accept.png"
+theme.cancel = config_dir .. "/themes/ghosty/icons/cancel.png"
+theme.calendar = config_dir .. "/themes/ghosty/icons/calendar.png"
+theme.task = config_dir .. "/themes/ghosty/icons/task.png"
+theme.tasks = config_dir .. "/themes/ghosty/icons/tasks.png"
+theme.task_done = config_dir .. "/themes/ghosty/icons/task_done.png"
+theme.project = config_dir .. "/themes/ghosty/icons/project.png"
+theme.udisks_glue = config_dir .. "/themes/ghosty/icons/udisk-glue.png"
+theme.usb = config_dir .. "/themes/ghosty/icons/usb.png"
+theme.cdrom = config_dir .. "/themes/ghosty/icons/cdrom.png"
 return theme
